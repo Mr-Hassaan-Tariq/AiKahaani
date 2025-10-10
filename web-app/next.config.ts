462b3d462442c60cedd448fbc80d62143fcf05e5
Muhammad Haris <harisbro88@gmail.com>
2025-10-11 01:24:24 +0500

    layout

--- web-app/next.config.ts
+++ web-app/next.config.ts
@@ -1,5 +1,8 @@
 module.exports = {
   images: {
-    domains: ['tubegenius-backend-production-05e9.up.railway.app'],
+    domains: [
+      'tubegenius-backend-production-05e9.up.railway.app',
+      'tubegenius-production.up.railway.app',
+    ],
   },
 };
